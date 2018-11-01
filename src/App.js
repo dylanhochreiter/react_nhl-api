cffcf60f845af3c519a6b2f9cbac98ab89cdef59
dylanhochreiter <dyl_hoch@live.com>
2018-11-01 15:49:10 -0400

    Add basic styling and background texture.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Teams />
+        <h1>NHL React Application</h1>
+        <div id="teams">
+          <h2>Teams</h2>
+          <p>Select a team to see players for that team and their stats:</p>
+          <Teams />
+        </div>
+        <div>
+          <h2>Players</h2>
+          <p>Below, players of the above selected team are displayed with stats and information.</p>
+        </div>
       </div>
     );
   }
